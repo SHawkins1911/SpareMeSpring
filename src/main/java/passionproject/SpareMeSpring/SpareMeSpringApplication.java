7fe6fb0f4ae9a9fbe5e26bb2ef9e05d855112297
Stefun Hawkins <stefunhawkins@zipcoders-MacBook-Pro.local>
2019-09-04 02:23:15 -0400

    new project setup

---
+++ src/main/java/passionproject/SpareMeSpring/SpareMeSpringApplication.java
@@ -0,0 +1,13 @@
+package passionproject.SpareMeSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpareMeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpareMeSpringApplication.class, args);
+	}
+
+}
