1777584a7d1747632611e24ecb375278cfd20336
Stefun Hawkins <stefunhawkins@Stefuns-MacBook-Pro.local>
2019-10-01 17:43:33 -0400

    add WebSecurity Config

---
+++ src/main/java/config/WebSecurityConfig.java
@@ -0,0 +1,72 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //Cross-origin-resource-sharing
+        http.cors().and()
+                .authorizeRequests()
+                //These are public pages
+                .antMatchers("/resources/**", "/error", "/api/user/**").permitAll()
+                //Only accessible to ADMIN role
+                .antMatchers("/api/admin/**").hasRole("ADMIN")
+                //all remaining paths should need authentication
+                .anyRequest().fullyAuthenticated()
+                .and()
+                //logout will log the user out by invalidating session
+                .logout().permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/api/user/logout", "POST")).and()
+                //login form and path
+                .formLogin().loginPage("/api/user/login").and()
+                //enable basic authentication Http header:basic username:password
+                .httpBasic().and()
+                //Cross side request forgery
+                .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return super.userDetailsService();
+    }
+
+    //Cross origin resource sharing
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
+    }
+}
