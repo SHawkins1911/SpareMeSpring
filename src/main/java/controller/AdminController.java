9125ba65f66b61017609047278ddb283db82ae80
Stefun Hawkins <stefunhawkins@Stefuns-MacBook-Pro.local>
2019-10-01 16:39:15 -0400

    add AdminController and UserDetailsImp for Spring Securityl

---
+++ src/main/java/controller/AdminController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.UserService;
+
+@RestController
+public class AdminController {
+
+        @Autowired
+    private UserService userService;
+
+    @GetMapping("/api/admin/all")
+    public ResponseEntity<?> findAllUsers(){
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+
+}
